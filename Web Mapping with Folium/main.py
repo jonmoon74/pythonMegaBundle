c8535af17ee1dd5bc268f68aea2a1fc3e4b29a62
jonmoon74 <jon.moon@outlook.com>
2019-06-16 20:39:34 +0100

    Finalising core map programme (upgrades to follow)

--- Web Mapping with Folium/main.py
+++ Web Mapping with Folium/main.py
@@ -1,4 +1,25 @@
 import folium
+import pandas as pd
 
-map = folium.Map(location=[45.372, -121.697], zoom_start=10, tiles='Stamen Terrain')
-map.create_map(path='test.html')
+df = pd.read_csv('Volcanoes_USA.txt')
+
+map = folium.Map(location=[df['LAT'].mean(), df['LON'].mean()], zoom_start=6, tiles='Stamen Terrain')
+
+
+def color(elev):
+    minimum = int(min(df['ELEV']))
+    step = int((max(df['ELEV']) - min(df['ELEV'])) / 3)
+    if elev in range(minimum, minimum + step):
+        col = 'green'
+    elif elev in range(minimum + step, minimum + step * 2):
+        col = 'orange'
+    else:
+        col = 'red'
+    return col
+
+
+for lat, lon, name, elev in zip(df['LAT'], df['LON'], df['NAME'], df['ELEV']):
+    folium.Marker(location=[lat, lon], popup=name + "\n(" + str(elev) + ")", icon=folium.Icon(color=color(elev))).add_to(map)
+
+
+map.save('test.html')
