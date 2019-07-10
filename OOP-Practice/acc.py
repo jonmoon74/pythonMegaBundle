493d17bae3af9c597c98270cdcb6ec577c0e64ec
jonmoon74 <jon.moon@outlook.com>
2019-07-10 20:55:00 +0100

    Finishing OOP practice, added own commit line to all methods

--- OOP-Practice/acc.py
+++ OOP-Practice/acc.py
@@ -1,4 +1,5 @@
 class Account:
+    """ This is the class to create a basic default account object"""
 
     def __init__(self, filepath):
         self.filepath = filepath
@@ -7,19 +8,34 @@ class Account:
 
     def withdraw(self, amount):
         self.balance = self.balance - amount
+        self.commit()
 
     def deposit(self, amount):
         self.balance = self.balance + amount
+        self.commit()
 
     def commit(self):
         with open(self.filepath, 'w') as file:
             file.write(str(self.balance))
 
 
+class Checking(Account):
+    """ This class creates checking account objects"""
+
+    def __init__(self, filepath, fee):
+        Account.__init__(self, filepath)
+        self.fee = fee
+
+    def transfer(self, amount):
+        self.balance = self.balance - amount - self.fee
+        self.commit()
+
+
 account = Account("balance.txt")
 print(account.balance)
 
-account.deposit(70)
-print(account.balance)
 
-account.commit()
+checking = Checking("balance.txt", 2)
+checking.transfer(108)
+
+print("Checking: " + str(checking.balance))
